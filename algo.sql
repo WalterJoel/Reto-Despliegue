13cecf862542f9bf078d935d9b7fc00c74c5e405
WalterJoel <walter.valdivia@ucsp.edu.pe>
2022-09-07 18:06:53 -0500

    archivos

---
+++ algo.sql
@@ -0,0 +1,3 @@
+use mydb;
+create table nombre (nombre varchar(255));
+insert into nombre values ('dato desde el dockerfile');
---
+++ server.js
@@ -0,0 +1,40 @@
+var mysql = require('mysql2');
+
+//
+const express = require('express'); 
+
+const port ='8080';
+const host ='0.0.0.0';
+const app = express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "secret",
+  database:"mydb",
+  port: "33061"
+
+});
+//Ruta por defecto
+app.get('/',(req,res)=>{    
+    res.send('Hello Krowdy')
+})
+
+//Ruta por defecto
+app.get('/registrar/:nombre',(req,res)=>{    
+    let nombre = req.params.nombre;
+    res.send('Hello World: ');
+    console.log(nombre);
+    var sql = `INSERT INTO nombre (nombre) VALUES ("${nombre}")`;
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 nombre nuevo insertado");
+    });
+})
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Conectado a la BD!");
+});
+
+app.listen(port,host);
